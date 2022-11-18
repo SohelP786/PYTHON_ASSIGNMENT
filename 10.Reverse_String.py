559df860137c19db8251fc90acd0e44b4ae3ace8
spatel8888 <117073230+spatel8888@users.noreply.github.com>
2022-11-18 21:49:48 +0530

    Add files via upload

---
+++ 10.Reverse_String.py
@@ -0,0 +1,5 @@
+a=input("Enter the String")
+A=""
+for i in a:
+    A=i+A
+print("String in reverse order",A)
---
+++ 15.even_length_word.py
@@ -0,0 +1,5 @@
+A=input("Enter the String")
+B=A.split(" ")
+for i in A:
+  if len(i)%2==0:
+    print("Words with even lenght:",i)
