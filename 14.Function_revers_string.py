3f016410743ea24996033ee08a31c677aa5cc575
spatel8888 <117073230+spatel8888@users.noreply.github.com>
2023-01-01 12:39:27 +0530

    Delete 14.Function_revers_string.py

--- 14.Function_revers_string.py
+++
@@ -1,8 +0,0 @@
-def reverse(A):
-    s = " "
-    for i in A:
-        s = i + s
-    return s
-A=input("Enter the String to reverse:")
-print("The original string is:",A)
-print("The reverse string is:", reverse(A)) 
